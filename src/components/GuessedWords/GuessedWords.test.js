e2b353a24884daae1861761328d080112ef7ed77
ckifer <ck108860@yahoo.com>
2020-02-14 15:43:41 -0800

    tests and code for non-zero guessed words

--- src/components/GuessedWords/GuessedWords.test.js
+++ src/components/GuessedWords/GuessedWords.test.js
@@ -48,4 +48,29 @@ describe('if no words guessed', () => {
   });
 });
 
-describe('if words are guessed', () => {});
+describe('if words are guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    {guessedWord: 'train', letterMatchCount: 3},
+    {guessedWord: 'agile', letterMatchCount: 1},
+    {guessedWord: 'party', letterMatchCount: 5}
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders guessed words section without error', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('display correct number of guessed words', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
+});
